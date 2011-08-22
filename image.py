d038d0098d0b09d7a7c27575d34ea57b6feb3c11
homm <homm86@gmail.com>
2011-08-22 11:49:29 +0600

    More optimization in paste_composite.

--- image.py
+++ image.py
@@ -5,15 +5,14 @@ from PIL import Image, ImageMath
 PALETTE_MODES = ('P',)
 
 def paste_composite(original, paste):
-    original.load()
-    paste.load()
+    # this faster then split()[-1]
+    image_alpha = paste._new(paste.getdata(3))
 
-    image_alpha = paste.split()[-1]
+    # this faster then split()[-1]
+    alpha_chanel = original._new(original.getdata(3))
+    alpha_chanel.paste(PIL.Image.new('L', alpha_chanel.size, 255), image_alpha)
 
-    alpha_chanel = original.split()[-1]
-    alpha_chanel.paste(Image.new('L', alpha_chanel.size, 255), image_alpha)
-
-    blending_chanel = ImageMath.eval("convert(a * 255 / b, 'L')", 
+    blending_chanel = PIL.ImageMath.eval("convert(a * 255 / b, 'L')", 
         a=image_alpha, b=alpha_chanel)
 
     original.paste(paste, (0, 0), blending_chanel)
